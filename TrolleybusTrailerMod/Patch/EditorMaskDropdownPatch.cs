0212296c579de17bca82105abb7a306fa80cf77f
krzychu124 <krzychu124@gmail.com>
2020-11-02 23:58:54 +0100

    Cleanup

--- TrolleybusTrailerMod/Patch/EditorMaskDropdownPatch.cs
+++ TrolleybusTrailerMod/Patch/EditorMaskDropdownPatch.cs
@@ -11,7 +11,6 @@ namespace TrolleybusTrailerMod.Patch {
         [HarmonyPatch(typeof(EditorUserFlagAttribute), MethodType.Constructor, new []{typeof(string), typeof(Type[])})]
         public static void Postfix(EditorUserFlagAttribute __instance, string displayName, params Type[] allowedTypes) {
             if (displayName.Equals("Right Pole") || displayName.Equals("Left Pole")) {
-                Debug.Log($"Is pole flag [{displayName}] length {allowedTypes.Length}, types: [{string.Join(";", allowedTypes.Select(t => t.Name).ToArray())}]");
                 FieldInfo allowedTypesFieldInfo = typeof(EditorUserFlagAttribute).GetField("m_AllowedTypes", BindingFlags.Instance | BindingFlags.NonPublic);
                 Type[] value = (Type[])allowedTypesFieldInfo.GetValue(__instance);
                 value = value.AddItem(typeof(TrolleybusTrailerAI)).ToArray();
